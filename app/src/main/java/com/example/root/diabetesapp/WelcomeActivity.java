b31fe84d69b7fa4d5bfc3773b432ee1e1ded7eb3
Gerardo Moxca Ruiz <gerardo.mx.rz@gmail.com>
2016-11-24 01:38:58 -0600

    Left Drawer tested

--- app/src/main/java/com/example/root/diabetesapp/WelcomeActivity.java
+++ app/src/main/java/com/example/root/diabetesapp/WelcomeActivity.java
@@ -2,7 +2,9 @@ package com.example.root.diabetesapp;
 
 import android.os.Bundle;
 import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -15,23 +17,58 @@ public class WelcomeActivity extends AppCompatActivity {
     SharedPreference shared;
     private String[] mPlaneTitles;
     private DrawerLayout mDrawerLayout;
+    private ActionBarDrawerToggle mDrawerToggle;
     private ListView mDrawerList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+
+        showToolbar("", false);
+
         mydb = new DBHelper(this);
         shared = new SharedPreference();
         ((TextView) findViewById(R.id.welcome_username)).setText(shared.getValue(this, "name"));
 
         mPlaneTitles = getResources().getStringArray(R.array.left_drawer);
         mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+
         mDrawerList = (ListView) findViewById(R.id.left_drawer);
 
         mDrawerList.setAdapter(new ArrayAdapter<String>(this,R.layout.drawer_list_item, mPlaneTitles));
         mDrawerList.setOnItemClickListener(new DrawerItemClickListener());
 
+        /*
+        mDrawerToggle = new ActionBarDrawerToggle(
+                this,
+                mDrawerLayout,
+                R.drawable.ic_drawer,
+                R.string.drawer_open,
+                R.string.drawer_close
+        ) {
+
+             Called when a drawer has settled in a completely closed state.
+            public void onDrawerClosed(View view) {
+                super.onDrawerClosed(view);
+                getActionBar().setTitle("Menu");
+            }
+
+
+             Called when a drawer has settled in a completely open state.
+            public void onDrawerOpened(View drawerView) {
+                super.onDrawerOpened(drawerView);
+                getActionBar().setTitle("");
+            }
+        };
+
+
+        // Set the drawer toggle as the DrawerListener
+        mDrawerLayout.addDrawerListener(mDrawerToggle);
+
+        getActionBar().setDisplayHomeAsUpEnabled(true);
+        getActionBar().setHomeButtonEnabled(true);
+        */
 
     }
 
@@ -44,6 +81,13 @@ public class WelcomeActivity extends AppCompatActivity {
 
     private void selectItem(int position) {
         Toast.makeText(this,"Position: "+position,Toast.LENGTH_SHORT).show();
+    }
+
+    public void showToolbar(String tittle, boolean upButton){
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle(tittle);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(upButton);
     }
 
 
