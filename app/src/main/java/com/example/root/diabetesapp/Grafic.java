15a80f0d2c877f12d334c6388883607729f7e29b
Aster December <Aster December>
2016-11-24 03:37:12 -0600

    grafica

--- app/src/main/java/com/example/root/diabetesapp/Grafic.java
+++ app/src/main/java/com/example/root/diabetesapp/Grafic.java
@@ -1,13 +1,77 @@
 package com.example.root.diabetesapp;
 
+import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.DashPathEffect;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+import java.util.Arrays;
+
+import android.content.Intent;
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.graphics.DashPathEffect;
+import android.os.Bundle;
+import android.view.View;
+
+import com.androidplot.util.PixelUtils;
+import com.androidplot.xy.SimpleXYSeries;
+import com.androidplot.xy.XYSeries;
+import com.androidplot.xy.*;
+import java.util.Arrays;
 
 public class Grafic extends AppCompatActivity {
 
+    private XYPlot plot;
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_grafic);
+
+
+
+        // initialize our XYPlot reference:
+        plot = (XYPlot) findViewById(R.id.plot);
+
+        // create a couple arrays of y-values to plot:
+        Number[] series1Numbers = {1, 4, 2, 8, 4, 16, 8, 32, 16, 64};
+
+
+        // turn the above arrays into XYSeries':
+        // (Y_VALS_ONLY means use the element index as the x value)
+        XYSeries series1 = new SimpleXYSeries(Arrays.asList(series1Numbers),
+                SimpleXYSeries.ArrayFormat.Y_VALS_ONLY, "Series1");
+
+
+
+        // create formatters to use for drawing a series using LineAndPointRenderer
+        // and configure them from xml:
+        LineAndPointFormatter series1Format = new LineAndPointFormatter(Color.RED, Color.GREEN, Color.BLUE, null);
+
+
+
+
+        // just for fun, add some smoothing to the lines:
+        // see: http://androidplot.com/smooth-curves-and-androidplot/
+        series1Format.setInterpolationParams(
+                new CatmullRomInterpolator.Params(10, CatmullRomInterpolator.Type.Centripetal));
+
+
+
+        // add a new series' to the xyplot:
+        plot.addSeries(series1, series1Format);
+
+
+        // reduce the number of range labels
+        plot.setTicksPerRangeLabel(3);
+
+        // rotate domain labels 45 degrees to make them more compact horizontally:
+        plot.getGraphWidget().setDomainLabelOrientation(-45);
     }
+
 }
