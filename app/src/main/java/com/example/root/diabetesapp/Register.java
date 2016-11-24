f014092f6af3670e0063ed32bc27812c22f25b67
ismael <ismaharo18@gmail.com>
2016-11-23 22:35:30 -0600

    Agregado registro de usuarios

--- app/src/main/java/com/example/root/diabetesapp/Register.java
+++ app/src/main/java/com/example/root/diabetesapp/Register.java
@@ -1,10 +1,14 @@
 package com.example.root.diabetesapp;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class Register extends AppCompatActivity {
+    DBHelper mydb;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,7 +26,36 @@ public class Register extends AppCompatActivity {
         getSupportActionBar().setDisplayHomeAsUpEnabled(upButton);
     }
 
+    public void registerUser(View view){
+        String name         = ((EditText) findViewById(R.id.name_register)).getText().toString();
+        String email        = ((EditText) findViewById(R.id.email_register)).getText().toString();
+        String password     = ((EditText) findViewById(R.id.password_register)).getText().toString();
+        String weight       = ((EditText) findViewById(R.id.weight_register)).getText().toString();
+        String height       = ((EditText) findViewById(R.id.height_register)).getText().toString();
+        String doctor_email = ((EditText) findViewById(R.id.doctor_email_register)).getText().toString();
+        String doctor_cel   = ((EditText) findViewById(R.id.doctor_cel_register)).getText().toString();
+        String doctor_name  = ((EditText) findViewById(R.id.doctor_name_register)).getText().toString();
+
+        /*
+         * INSERT USER
+         */
+        boolean res =  mydb.insertUser(name, email, password, weight, height, doctor_email, doctor_cel, doctor_name);
+
+        /*
+         * Check if user was inserted
+         */
+        if(res){
+            Toast.makeText(getApplicationContext(), "Usuario: "+name+"Registrado", Toast.LENGTH_SHORT).show();
+            
+
+        }else{
+            Toast.makeText(getApplicationContext(), "ERROR AL REGISTRAR", Toast.LENGTH_SHORT).show();
+        }
+
+    }
+
 }
+
 
 
 
