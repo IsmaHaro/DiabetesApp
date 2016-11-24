ce782d432e5c2b2780ff7759c2ada26cc9d88360
Gerardo Moxca Ruiz <gerardo.mx.rz@gmail.com>
2016-11-23 21:13:10 -0600

    Added Toolbar, not working

--- app/src/main/java/com/example/root/diabetesapp/Register.java
+++ app/src/main/java/com/example/root/diabetesapp/Register.java
@@ -2,6 +2,7 @@ package com.example.root.diabetesapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 public class Register extends AppCompatActivity {
 
@@ -9,5 +10,20 @@ public class Register extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        showToolbar("Registro", true);
+
     }
+
+    public void showToolbar(String tittle, boolean upButton){
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle(tittle);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(upButton);
+    }
+
 }
+
+
+
+
