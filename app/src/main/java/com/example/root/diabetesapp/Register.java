df7f5e5d4db823731a1b0d9d8eb6bded3ad88fae
Gerardo Moxca Ruiz <gerardo.mx.rz@gmail.com>
2016-11-23 23:06:31 -0600

    A very brief verification of user in registration

--- app/src/main/java/com/example/root/diabetesapp/Register.java
+++ app/src/main/java/com/example/root/diabetesapp/Register.java
@@ -1,5 +1,6 @@
 package com.example.root.diabetesapp;
 
+import android.database.Cursor;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
@@ -30,30 +31,41 @@ public class Register extends AppCompatActivity {
         /*
          * RETRIEVE USER INFORMATION
          */
-        String name         = ((EditText) findViewById(R.id.name_register)).getText().toString();
         String email        = ((EditText) findViewById(R.id.email_register)).getText().toString();
-        String password     = ((EditText) findViewById(R.id.password_register)).getText().toString();
-        String weight       = ((EditText) findViewById(R.id.weight_register)).getText().toString();
-        String height       = ((EditText) findViewById(R.id.height_register)).getText().toString();
-        String doctor_email = ((EditText) findViewById(R.id.doctor_email_register)).getText().toString();
-        String doctor_cel   = ((EditText) findViewById(R.id.doctor_cel_register)).getText().toString();
-        String doctor_name  = ((EditText) findViewById(R.id.doctor_name_register)).getText().toString();
+
+        Cursor cursor = mydb.getUserByEmail(email);
+
+        if (cursor != null){
+
+            Toast.makeText(this, "El usuario ya existe", Toast.LENGTH_SHORT).show();
+
+        }else{
+            String name         = ((EditText) findViewById(R.id.name_register)).getText().toString();
+            String password     = ((EditText) findViewById(R.id.password_register)).getText().toString();
+            String weight       = ((EditText) findViewById(R.id.weight_register)).getText().toString();
+            String height       = ((EditText) findViewById(R.id.height_register)).getText().toString();
+            String doctor_email = ((EditText) findViewById(R.id.doctor_email_register)).getText().toString();
+            String doctor_cel   = ((EditText) findViewById(R.id.doctor_cel_register)).getText().toString();
+            String doctor_name  = ((EditText) findViewById(R.id.doctor_name_register)).getText().toString();
 
         /*
          * INSERT USER
          */
-        boolean res =  mydb.insertUser(name, email, password, weight, height, doctor_email, doctor_cel, doctor_name);
+            boolean res =  mydb.insertUser(name, email, password, weight, height, doctor_email, doctor_cel, doctor_name);
 
         /*
          * Check if user was inserted
          */
-        if(res){
-            Toast.makeText(getApplicationContext(), "Usuario: "+name+"Registrado", Toast.LENGTH_SHORT).show();
+            if(res){
+                Toast.makeText(getApplicationContext(), "Usuario: "+name+"Registrado", Toast.LENGTH_SHORT).show();
 
 
-        }else{
-            Toast.makeText(getApplicationContext(), "ERROR AL REGISTRAR", Toast.LENGTH_SHORT).show();
+            }else{
+                Toast.makeText(getApplicationContext(), "ERROR AL REGISTRAR", Toast.LENGTH_SHORT).show();
+            }
         }
+
+
 
     }
 
