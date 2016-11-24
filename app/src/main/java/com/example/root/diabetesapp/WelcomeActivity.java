ee04300913cfa867728dfe2e3ca5e8bf9b251376
ismael <ismaharo18@gmail.com>
2016-11-24 11:12:01 -0600

    Agregado funcion para meses

--- app/src/main/java/com/example/root/diabetesapp/WelcomeActivity.java
+++ app/src/main/java/com/example/root/diabetesapp/WelcomeActivity.java
@@ -252,4 +252,20 @@ public class WelcomeActivity extends AppCompatActivity
     }
 
 
+    private String getMonth(int m){
+        if( m == 1 ) return "Ene";
+        else if( m == 2 ) return "Feb";
+        else if( m == 3 ) return "Mar";
+        else if( m == 4 ) return "Abr";
+        else if( m == 5 ) return "May";
+        else if( m == 6 ) return "Jun";
+        else if( m == 7 ) return "Jul";
+        else if( m == 8 ) return "Ago";
+        else if( m == 9 ) return "Sep";
+        else if( m == 10 ) return "Oct";
+        else if( m == 11 ) return "Nov";
+            //else if( m == 12 ) return "Dic";
+        else return "Dic";
+    }
+
 }
