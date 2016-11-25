b0030da05faaeba1a6a56098cfffc69e64a26861
Gerardo Moxca Ruiz <gerardo.mx.rz@gmail.com>
2016-11-24 19:54:10 -0600

    Trying to fix Drawer

--- app/src/main/java/com/example/root/diabetesapp/Profile.java
+++ app/src/main/java/com/example/root/diabetesapp/Profile.java
@@ -16,6 +16,8 @@ public class Profile extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
+
         mydb = new DBHelper(this);
         shared = new SharedPreference();
         showToolbar("Perfil", true);
--- app/src/main/java/com/example/root/diabetesapp/WelcomeActivity.java
+++ app/src/main/java/com/example/root/diabetesapp/WelcomeActivity.java
@@ -25,7 +25,7 @@ public class WelcomeActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
     DBHelper mydb;
     SharedPreference shared;
-    private DrawerLayout mDrawerLayout;
+    protected DrawerLayout mDrawerLayout;
     private ActionBarDrawerToggle mDrawerToggle;
     private ListView mDrawerList;
 
@@ -33,8 +33,6 @@ public class WelcomeActivity extends AppCompatActivity
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
-
-        //showToolbar("", false);
 
         mydb = new DBHelper(this);
         shared = new SharedPreference();
@@ -81,6 +79,7 @@ public class WelcomeActivity extends AppCompatActivity
             case R.id.nav_profile:
                 intent = new Intent(this, Profile.class);
                 startActivity(intent);
+                mDrawerLayout.closeDrawers();
                 break;
 
             case R.id.nav_grafic:
